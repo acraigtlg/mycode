4e998e24656d4bed61eff708d3aee629d7cc64b4
acraigtlg <aa.t.craig@gmail.com>
2023-07-06 15:43:51 +0000

    about complex lists

---
+++ challenge_20.py
@@ -0,0 +1,30 @@
+# create wordbank list
+wordbank = ["indentation", "spaces"]
+
+
+
+# create student list
+tlgstudents= ['Albert', 'Anthony', 'Brenden', 'Craig', 'Deja', 'Elihu', 'Eric', 'Giovanni', 'James', 'Joshua', 'Maria', 'Mohamed', 'PJ', 'Philip', 'Sagan', 'Suchit', 'Meka', 'Trey', 'Winton', 'Xiuxiang', 'Yaping']
+
+
+# add integer 4 to wordbank
+wordbank.append(4)
+
+
+# prompt user for number 0-20 be sure to convert to int
+num= int(input("Pick a student number!"))
+
+
+
+# use number from user to slice list
+choice= int(input("Pick a student number!"))
+student_name= tlgstudents[choice]
+
+
+
+
+
+# print output
+
+
+
