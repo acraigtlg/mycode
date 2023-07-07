1dd54e1dff4e67d4480906e9c41ef1635f799346
acraigtlg <aa.t.craig@gmail.com>
2023-07-07 17:25:38 +0000

    Playing with python pip packages

---
+++ miniproject35.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+"""Establishing This Year's Travel Plans"""
+
+
+season = input("What is your favorite season?")
+
+
+if season = "summer":
+    print("Adventures in sunny San Diego, CA await you!!!"
+
+if season = "spring":
+    print("You'd really enjoy the scenery and history of Charleston, SC."
