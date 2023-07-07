d386d92127b110113d6936b0bff107d7f42d6ccd
acraigtlg <aa.t.craig@gmail.com>
2023-07-07 17:55:06 +0000

    lab35 miniproject

--- miniproject35.py
+++ miniproject35.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
-
-
 """Establishing This Year's Travel Plans"""
 
 
-season = input("What is your favorite season?")
+print('What is your favorite season? ')
 
+season = input()
+if season == "summer":
+    print("Adventures in sunny San Diego, CA await you!!!")
 
-if season = "summer":
-    print("Adventures in sunny San Diego, CA await you!!!"
+if season == "spring":
+    print("You'd really enjoy the scenery and history of Charleston, SC.")
 
-if season = "spring":
-    print("You'd really enjoy the scenery and history of Charleston, SC."
+if season == "fall":
+    print("The views of the autumn leaves in Ashville, NC will take your breath away.")
+
+if season == "winter":
+    print("You should consider your life choices.")
+
